0aacfef9b73ae623c691b6b40027ed8e2299ad1b
satis.vishnu <satis.vishnu@gmail.com>
2016-12-10 01:00:10 +0530

    Fixed some minor bugs found during end to end testing.

--- api/services/user_service.py
+++ api/services/user_service.py
@@ -4,24 +4,22 @@ from api.common_helper.common_utils import CommonHelper
 
 
 class UserService:
-
     def __init__(self):
         pass
 
     @staticmethod
     def create_user_and_add_to_niche(email=None,
-                    password=None,
-                    first_name=None,
-                    last_name=None,
-                    company=None):
+                                     password=None,
+                                     first_name=None,
+                                     last_name=None,
+                                     company=None):
         user = UserAdapter.create(user_guid=CommonHelper.generate_guid(),
-                           email=email,
-                           password=password,
-                           first_name=first_name,
-                           last_name=last_name,
-                           company=company)
+                                  email=email,
+                                  password=password,
+                                  first_name=first_name,
+                                  last_name=last_name,
+                                  company=company)
         AccountsService.add_user_to_niche(user=user)
-
 
     @staticmethod
     def get_user_by_email(email=None):
