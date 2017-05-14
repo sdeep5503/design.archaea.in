5ec695895ad91d4e9ad5e15f25cf9e4066f15ab5
vizsatiz <satis.vishnu@gmail.com>
2017-05-14 23:26:39 +0530

    Removing network calls from master ctrl and moving them to module file

--- api/templates/src/js/controllers/master-ctrl.js
+++ api/templates/src/js/controllers/master-ctrl.js
@@ -13,22 +13,16 @@ function MasterCtrl($scope, $rootScope, $cookieStore, $window, Whoami, Accounts)
     $scope.user = {};
     $scope.accounts = [];
     $scope.currentAccount = {};
-    $rootScope.current = {};
     $scope.currentPageName = 'Nerd Console';
     $scope.currentPageDescription = 'Manage all your nerd clouds and apps give life to your apps';
 
-    Whoami.get(function(response) {
-        $scope.user = response;
-    }, function(error) {
-        console.log(error.data.message);
+    $scope.$on('GetAccountsSuccess', function() {
+        $scope.accounts = $rootScope.accounts;
+        $scope.currentAccount = $rootScope.current.account;
     });
 
-    Accounts.get(function(response) {
-        $scope.accounts = response;
-        $rootScope.current.account = $scope.accounts[0];
-        $scope.currentAccount = $scope.accounts[0];
-    }, function() {
-        console.log(error.data.message);
+    $scope.$on('WhoAmICallSuccess', function() {
+        $scope.user = $rootScope.current.user;
     });
 
     $scope.getWidth = function() { return window.innerWidth; };
@@ -71,32 +65,7 @@ function MasterCtrl($scope, $rootScope, $cookieStore, $window, Whoami, Accounts)
 
     $scope.changePageInfoHeader = function (pageName)
     {
-      /**var url = $window.location.href;
-      var urlPath = url.split('/');
-      var moduleName = urlPath[urlPath.length - 1]
 
-      var headerMap = {
-        'dashboard': 'Dashboard',
-        'nerds': 'Manage Nerds',
-        'users': 'Manage Users',
-        'tables': 'Manage Tables',
-        'applications': 'Manage Applications',
-        'createUpdateApp': 'Create/Update Applications',
-        'documentations': 'Documentations'
-      }
-
-      var descriptionMap = {
-        'dashboard': 'Latest updates from Nerdstacks',
-        'nerds': 'Manage all your nerd clouds and use them to give life to your apps',
-        'users': 'Manage all the users in this account',
-        'tables': 'Manage Tables',
-        'applications': 'Manage Applications',
-        'createUpdateApp': 'Create/Update Applications',
-        'documentations': 'All the important documentations that you need'
-      }
-
-      $scope.currentPageName = headerMap[pageName ? pageName : moduleName];
-      $scope.currentPageDescription = descriptionMap[pageName ? pageName : moduleName];**/
     }
 
     $scope.logout = function () {
